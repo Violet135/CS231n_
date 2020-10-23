f340c0a850656efa32c238dcc3fefbe9807dd9b2
violet135 <fatemenajafi135@gmail.com>
2020-10-23 14:49:40 +0330

    'test'

--- Assignment1/cs231n/classifiers/linear_svm.py
+++ Assignment1/cs231n/classifiers/linear_svm.py
@@ -60,7 +60,14 @@ def svm_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    # pass
+    mask = np.zeros(margins.shape)
+    mask [margins > 0] = 1
+    row_sum = np.sum(mask, axis=1)
+    mask[np.arange(num_train), y]= -row_sum.T
+    dW = np.dot(X.T, mask)
+    dW /= num_train
+    dW += 2 * reg * W
+
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     
